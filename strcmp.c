af69be254c55370add484cb1eb2aaa955f40fe61
marufhasan566 <marufhasan.mh1@gmail.com>
2023-05-10 21:14:58 +0600

    strcmp added

---
+++ strcmp.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char a[100], b[100];
+    scanf("%s %s", a, b);
+    int result = strcmp(a, b);
+    // printf("%d", result);
+    if (result == 0)
+    {
+        printf("A & B both are same");
+    }
+    else if (result > 0)
+    {
+        printf("B is smaller");
+    }
+    else if (result < 0)
+    {
+        printf("A is smaller");
+    }
+    return 0;
+}
